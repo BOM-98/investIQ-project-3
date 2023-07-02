2a04da87e2d19c9ce7f16c22ae75e6e4cdbb0cff
BOM-98 <64743302+BOM-98@users.noreply.github.com>
2023-07-02 17:52:04 +0000

    feat: fetch S&P500 tickers & names

--- run.py
+++ run.py
@@ -1,20 +1,21 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
-from math import sqrt
+import pandas as pd
+import pandas_datareader.data as web
+import numpy as np
+from datetime import datetime
 
-def calculate_variance(dataset):
-  numerator = 0
-  mean = sum(dataset)/len(dataset)
+# URL of the Wikipedia page from which to scrape the S&P 500 company list
+url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
 
-  for number in dataset:
-    element = (number - mean)**2
-    numerator += element
+# Use pandas to scrape the tables from the Wikipedia page
+tables = pd.read_html(url)
 
-  variance = numerator / len(dataset)
-  return variance
+# The first table on the page is the one we want
+sp500_table = tables[0]
 
-def calculate_stddev(dataset):
-  variance = calculate_variance(dataset)
-  stddev = sqrt(variance)
-  return stddev
+# The ticker symbol is in the 'Symbol' column
+tickers = sp500_table[['Symbol', 'Security']]
+tickers_list = tickers.values.tolist()
+
+# Print the tickers and name
+for ticker in tickers_list:
+    print(ticker[0]+' - '+ticker[1])
