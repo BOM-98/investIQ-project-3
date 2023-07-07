631e86761bbe876995fc07ea6cadfcec4a51ac2f
BOM-98 <64743302+BOM-98@users.noreply.github.com>
2023-07-07 18:13:43 +0000

    docs: add comments to explain main logic

--- run.py
+++ run.py
@@ -13,53 +13,58 @@ from pypfopt import HRPOpt
 from utils import *
 
 def main():
+    # Step 1: Choosing Index
+    #Function to pick index
     chosen_index = get_companies_list()
+    #Function to scrape tickers from chosen index
     symbols = scrape_company_tickers(chosen_index)
+    #Function to scrape financial infromation from tickers that were scraped
     fundamentals_data = collect_data(symbols)
+    
+    # Step 2: Choosing stocks
+    #Function to calculate quarterly returns from each stock from our index
     fundamentals_data['quarterlyReturn'] = process_data(symbols)
     print ('----------------------------------------------------')
     typewriter('Here are the fundamentals for your list of companies: \n')
     print('----------------------------------------------------')
     print(fundamentals_data)
     print('')
+    #Function to provide information on each company stat / ratio
     fundamentals_information()
+    #Remove companies with missing data
     fundamentals_data_dropna = fundamentals_data.dropna()
+    #Calculate the number companies with missing data
     removed_companies = fundamentals_data['symbol'].count() - fundamentals_data_dropna['symbol'].count()
+    #Calculate the percentiles for each company statistic / ratio
     fundamentals_percentile = calculate_percentile_rank(fundamentals_data_dropna[['forwardPE', 'debtToEquity','forwardEps', 'returnOnEquity', 'returnOnAssets', 'revenueGrowth', 'quickRatio', 'quarterlyReturn']])
     fundamentals_percentile['symbols'] = symbols
     fundamentals_percentile = fundamentals_percentile[['symbols','forwardPE','debtToEquity','forwardEps', 'returnOnEquity', 'returnOnAssets', 'revenueGrowth', 'quickRatio', 'quarterlyReturn']]
-
     typewriter('----------------------------------------------------\n')
     typewriter('Here are your companies scored and ranked based on their fundamentals \n - fundamentals are displayed here as percentiles: \n - The investIQ alogorithm calculates company scores based on fundamentals \n')
     typewriter('----------------------------------------------------\n')
-
+    #Function to rank companies based on their fundamentals
     rank_stocks(fundamentals_percentile)
+    #Function to sort companies based on their scores
     fundamentals_percentile.sort_values('score', ascending = False, inplace = True)
     fundamentals_percentile = fundamentals_percentile[['symbols','score','forwardPE','debtToEquity','forwardEps', 'returnOnEquity', 'returnOnAssets', 'revenueGrowth', 'quickRatio', 'quarterlyReturn']]
     fundamentals_percentile = fundamentals_percentile.reset_index(drop=True)
     print(fundamentals_percentile)
     print('\n--------------------------------------------------------')
     print(str(removed_companies) + ' stocks removed due to missing data fields from Yahoo Finance')
-
+    
+    # Step 3: Optimize Portfolio
+    #Return the number of companies the user wants to include in their portfolio
     portfolio = choose_companies(fundamentals_percentile)
     portfolio_stocks = portfolio['symbols'].tolist()
+    #Return the historical closing prices for each stock in the portfolio
     portfolio_prices = combine_stocks(portfolio_stocks)
-
-    #Mean Variance Optimization 
-    # mu = mean_historical_return(portfolio_prices)
-    # S = CovarianceShrinkage(portfolio_prices).ledoit_wolf()
-    # ef = EfficientFrontier(mu, S)
-    # weights = ef.max_sharpe()
-    # cleaned_weights = ef.clean_weights()
-    # print(dict(cleaned_weights))
-    # ef.portfolio_performance(verbose=True)
     latest_prices = get_latest_prices(portfolio_prices)
-
+    
+    # Step 4: Allocating Budget
     #Hierarchical Risk Parity Optimization(HRP)
     hpp_optimization(portfolio_prices, latest_prices)
     reset_program()    
     main()
-
 
             
 typewriter("""\
